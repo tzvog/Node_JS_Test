869f5a27b8adda25a776a16a6378b368681490d9
tzvi <timothy.vogel@mail.huji.ac.il>
2021-08-29 19:46:26 +0300

    made the types global

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const responses = require('./response');
 
 // const variables 
 const cutoff_year = 2000; 
+const types = [chuck, kanye, name_parse]; 
 
 // local varriables
 var total_request_counter = {}; 
@@ -42,7 +43,7 @@ app.get('/api/surprise' ,(req, res) => {
         return;  
     }
 
-    chuck.response(success);
+    // chuck.response(success);
     // name_parse.response(req.query.name, success);
     // kanye.response(success);
 
@@ -70,21 +71,30 @@ app.get('/api/surprise' ,(req, res) => {
     // responses.fail(res); 
 });
 
+// returns the given stats 
+app.get('/api/stats' ,(req, res) => {
+
+    res.status(200).json(total_request_counter); 
+});
+
+// this function sets off initial uses for the code 
 function init()
 {
+    // sets the keys to default values 
     total_request_counter["requests"] = 0; 
     total_request_counter["distribution"] = []; 
-
-    types = [chuck.type, kanye.type, name_parse.type]; 
-    
+   
+    // for each type create a link in the dictionary and where the key is in the list 
     types.forEach((item, index, array) => {
-        total_request_counter["distribution"].push({"type": item, "count": 0}); 
-        distribution_keys[item] = index;
+        total_request_counter["distribution"].push({"type": item.type, "count": 0}); 
+        distribution_keys[item.type] = index;
     })
 }
 
+// calls the init function
 init();
 
+// starts listening on the port
 app.listen(3000, console.log("Listening on port"));
 
 
