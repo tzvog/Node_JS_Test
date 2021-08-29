899c7b828cbe463980dba23189028655d7ff0ce6
tzvi <timothy.vogel@mail.huji.ac.il>
2021-08-29 12:32:20 +0300

    get should work

--- app.js
+++ app.js
@@ -11,22 +11,22 @@ const responses = require('./response');
 
 // const variables 
 const cutoff_year = 2000; 
+
 const repspone_type = {
     TYPE1: 'chuck-norris-joke',
     TYPE2: 'kanye-quote',
     TYPE3: 'name-sum'
-}
-
+};
 
 // local varriables
-var request = {"requests": 0, "distribution": []};
+var request = {"requests": 0, "distribution": {}};
 
 
 app.get('/api/surprise' ,(req, res) => {
 
     function success(handle_res)
     {
-        res.status(200).json({Response: repspone_type.TYPE1, result: handle_res});
+        res.status(200).json({type: repspone_type.TYPE1, result: handle_res});
     }
 
     // checks if we the right amount of parameters and correct ones
@@ -48,7 +48,28 @@ app.get('/api/surprise' ,(req, res) => {
         return;  
     }
 
-    name_parse(req.query.name, success);
+    if(birth_year <= cutoff_year)
+    {
+        chuck(success); 
+        return; 
+    }
+    else
+    {
+        if(req.query.name[0] != 'A' && req.query.name[0] != 'Z')
+        {
+            kanye(success);
+            return;  
+        }
+    }
+
+    if(req.query.name[0] != 'Q')
+    {
+        name_parse(req.query.name, success);
+        return; 
+    }
+
+
+    responses.fail(res); 
 });
 
 app.listen(3000, console.log("Listening on port"));
